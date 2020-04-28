87b1be1b5ca8cddc60eaddbb4b42aa8f2c413c51
Lawa0921 <bag571ivy3470@gmail.com>
2020-04-28 13:44:57 +0800

    feature/task_2

--- index.js
+++ index.js
@@ -7,3 +7,23 @@ function onClick() {
   hb.classList.toggle("is-active");
   nav.classList.toggle("is-active");
 }
+
+let form = document.querySelector("form");
+let sumbitButton = document.querySelector(".button");
+
+sumbitButton.addEventListener("click", function (e) {
+  e.preventDefault();
+  let inputValues = form.elements;
+  fetch(
+    `https://still-spire-37210.herokuapp.com/positions.json?description=${inputValues.description.value}&localion=${inputValues.location.value}&full_time=${inputValues.full_time.checked}`
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      data.map((result) => {
+        console.log(result);
+      });
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
