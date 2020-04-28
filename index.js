8699b62b6f62dd53c69e3f2e07bb638c565b809f
Lawa0921 <bag571ivy3470@gmail.com>
2020-04-28 15:37:00 +0800

    feature/task_3

--- index.js
+++ index.js
@@ -10,20 +10,61 @@ function onClick() {
 
 let form = document.querySelector("form");
 let sumbitButton = document.querySelector(".button");
+let infoContainer = document.querySelector("table");
+let infoElement = document.querySelector("tr");
 
-sumbitButton.addEventListener("click", function (e) {
-  e.preventDefault();
-  let inputValues = form.elements;
+function getElement(result) {
+  let newElement = infoElement.cloneNode(true);
+
+  let job = newElement.querySelector("a");
+  job.textContent = result.title;
+  job.href = result.url;
+
+  let loc = newElement.querySelector(".location");
+  loc.textContent = result.location;
+
+  let company = newElement.querySelector(".company");
+  company.textContent = result.company;
+  company.href = result.company_url;
+
+  let fulltime = newElement.querySelector(".fulltime");
+  fulltime.textContent = result.type;
+
+  infoContainer.appendChild(newElement);
+}
+
+function fullTimeTrue(inputValues) {
   fetch(
     `https://still-spire-37210.herokuapp.com/positions.json?description=${inputValues.description.value}&localion=${inputValues.location.value}&full_time=${inputValues.full_time.checked}`
   )
     .then((response) => response.json())
     .then((data) => {
       data.map((result) => {
-        console.log(result);
+        getElement(result);
       });
-    })
-    .catch((error) => {
-      console.log(error);
     });
+}
+
+function fullTimeFalse(inputValues) {
+  fetch(
+    `https://still-spire-37210.herokuapp.com/positions.json?description=${inputValues.description.value}&localion=${inputValues.location.value}`
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      data.map((result) => {
+        getElement(result);
+      });
+    });
+}
+
+sumbitButton.addEventListener("click", function (e) {
+  e.preventDefault();
+  infoContainer.innerHTML = "";
+  let inputValues = form.elements;
+
+  if (inputValues.full_time.checked) {
+    fullTimeTrue(inputValues);
+  } else {
+    fullTimeFalse(inputValues);
+  }
 });
